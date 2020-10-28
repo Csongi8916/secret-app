f800dbe4cb8543e709443e58480181e147c13545
cskosolution <cskosolution@gmail.com>
2020-10-28 18:13:40 +0100

    change body parser

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ const secretRoutes = require('./routes/secret-route');
 
 const app = express();
 
- app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
-// app.use(bodyParser.json()); // application/json
+// app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
+ app.use(bodyParser.json()); // application/json
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
